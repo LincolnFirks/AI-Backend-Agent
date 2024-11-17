cce1b802fbc1c444ad4e18be14bb666b1d1c31aa
LincolnFirks <lincfirks@gmail.com>
2024-11-17 00:31:29 -0500

    update Readme

--- server/main.js
+++ server/main.js
@@ -2,14 +2,13 @@ import { Meteor } from 'meteor/meteor';
 import { CurrentStore } from '../imports/api/store';
 import { HistoryStore } from '../imports/api/store';
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import { prePrompt } from './prompt';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const prePrompt = "Reply to this message in json format only, nothing else. The json should have a type, a key and a value. The method will be one of: update, read, create or delete. The key will be what the user specifies.  The value will be the value to change the data for key too (can leave blank for delete and read). I want you to discern the key and value from the users request, which will be natural language. If you cannot discern a valid CRUD operation from the prompt, you should respond with a JSON with an error message only. If the user's key is multiple words, use only underscores to separate them. Make all keys lowercase. "
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-8b" });
-
+const model = genAI.getGenerativeModel({ model: process.env.GEMINI_MODEL });
 
 Meteor.methods({
     async GetJSON(userPrompt) {
@@ -19,7 +18,6 @@ Meteor.methods({
         try {
             const match = result.response.text().match(/```json\s*([\s\S]*?)\s*```/);
             if (match && match[1]) {
-                // Parse the extracted JSON
                 const parsed = JSON.parse(match[1]);
         
                 console.log(`Type: ${jsonData.type}`);
@@ -33,74 +31,102 @@ Meteor.methods({
         }
 
         if (parsed.error) {
-            return parsed.error;
+            parsed.method = '-';
+            parsed.key = '-';
+            parsed.value = '-';
+            return parsed;
         }
 
         const validMethods = ['update', 'read', 'create', 'delete'];
 
         if (!parsed.hasOwnProperty(method) || !parsed.hasOwnProperty(key) || !parsed.hasOwnProperty(value) || !validMethods.includes(parsed.method)) {
-            throw new Meteor.Error("Invalid JSON format.");
+            parsed.method = '-';
+            parsed.key = '-';
+            parsed.value = '-';
+            parsed.error = ("LLM JSON response was invalid");
         }
 
         return parsed;
     },
 
+    CallCRUD(result) {
 
-    
-
-    async create({ key, value }) {
+        if (!result.method) {
+            throw new Meteor.Error("invalid-arguments", "Method is required.");
+        }
         if (!key || !value) throw new Meteor.Error("invalid-arguments", "Key and value are required.");
-        const cursor = CurrentStore.findOneAsync({ key});
-        if (cursor) {
-          throw new Meteor.Error("invalid-arguments", "Document with this key exists already.");
-        }
-        const time = new Date()
-        doc = {
-          key,
-          value,
-          created: time,
-          updated: time
 
-        }
-        delete doc._id;
-        CurrentStore.insertAsync(doc);
+        const method = result.method;
 
-        HistoryStore.insertAsync({method: "create",key,value,time})
-    },
-    async update({ key, value }) {
-        if (!key || !value) throw new Meteor.Error("invalid-arguments", "Key and value are required.");
-        const cursor = await CurrentStore.findOneAsync({ key });
-        if (!cursor) {
-          throw new Meteor.Error("invalid-arguments", "No document with this key");
+        if (method === "create") {
+            Create(method.key,method.value);
+        } else if (method === "delete") {
+            Delete(method.key);
+        } else if (method === "update") {
+            Update(method.key,method.value);
+        } else if (method === "read") {
+            Read(method.key);
         }
-        const time = new Date();
-        const updatedDoc = {
-            $set: { value, updated: time } 
-        };
-        
-        CurrentStore.updateAsync(updatedDoc);
-        HistoryStore.insertAsync({method: "update",key,value,time})
-    },
-    async read(key) {
-        const cursor = await CurrentStore.findOneAsync({ key });
-        if (!cursor) {
-          throw new Meteor.Error("invalid-arguments", "No document with this key");
-        }
-        HistoryStore.insertAsync({method: "read",key,value: cursor.value,time: new Date()})
-        return cursor;
+    }
 
-    },   
-    async delete(key) {
-        const cursor = await CurrentStore.findOneAsync({ key });
-        if (!cursor) {
-          throw new Meteor.Error("invalid-arguments", "No document with this key");
-        }
-  
-        CurrentStore.removeAsync({key});
-        HistoryStore.insertAsync({method: "delete",key,value: cursor.value,time: new Date()})
-    },
+
 
 });
+
+async function Create({ key, value }) {
+    if (!key || !value) throw new Meteor.Error("invalid-arguments", "Key and value are required.");
+    const cursor = CurrentStore.findOneAsync({ key});
+    if (cursor) {
+      throw new Meteor.Error("invalid-arguments", "Document with this key exists already.");
+    }
+    const time = new Date()
+    doc = {
+      key,
+      value,
+      created: time,
+      updated: time
+
+    }
+    delete doc._id;
+    CurrentStore.insertAsync(doc);
+
+    HistoryStore.insertAsync({method: "create",key,value,time})
+}
+
+async function Update({ key, value }) {
+    if (!key || !value) throw new Meteor.Error("invalid-arguments", "Key and value are required.");
+    const cursor = await CurrentStore.findOneAsync({ key });
+    if (!cursor) {
+      throw new Meteor.Error("invalid-arguments", "No document with this key");
+    }
+    const time = new Date();
+    const updatedDoc = {
+        $set: { value, updated: time } 
+    };
+    
+    CurrentStore.updateAsync(updatedDoc);
+    HistoryStore.insertAsync({method: "update",key,value,time})
+}
+
+async function Read(key) {
+    const cursor = await CurrentStore.findOneAsync({ key });
+    if (!cursor) {
+      throw new Meteor.Error("invalid-arguments", "No document with this key");
+    }
+    HistoryStore.insertAsync({method: "read",key,value: cursor.value,time: new Date()})
+    return cursor;
+
+}
+
+async function Delete(key) {
+    const cursor = await CurrentStore.findOneAsync({ key });
+    if (!cursor) {
+      throw new Meteor.Error("invalid-arguments", "No document with this key");
+    }
+
+    CurrentStore.removeAsync({key});
+    HistoryStore.insertAsync({method: "delete",key,value: cursor.value,time: new Date()})
+}
 
 
 
